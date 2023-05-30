eeee41998ed1e276b7699c162dc1f97bc7021555
Ekt0 <ektomanagement@gmail.com>
2023-05-30 14:21:28 +0200

    clarifying code reading (space+ indent)

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,24 +3,29 @@
 let productLocalStorage = JSON.parse(localStorage.getItem("produit"));
 console.table(productLocalStorage);
 const positionEmptyCart = document.querySelector("#cart__items");
-  //Récupération des coordonnées du formulaire client
 
-  const firstNameElt = document.getElementById("firstName");
-  const lastNameElt = document.getElementById("lastName");
-  const adressElt = document.getElementById("address");
-  const cityElt = document.getElementById("city");
-  const mailElt = document.getElementById("email");
+//Récupération des coordonnées du formulaire client
+const firstNameElt  = document.getElementById("firstName");
+const lastNameElt   = document.getElementById("lastName");
+const adressElt     = document.getElementById("address");
+const cityElt       = document.getElementById("city");
+const mailElt       = document.getElementById("email");
+
+//Création des expressions régulières
+const charRegExp = new RegExp("^[a-zA-Z ,.'-]+$");
+console.log(firstNameElt);
 
 // Si le panier est vide
 function getCart() {
+
   if (productLocalStorage === null || productLocalStorage == 0) {
     const emptyCart = `Votre panier est vide`;
     positionEmptyCart.innerText = emptyCart;
   } else {
+
     for (let produit in productLocalStorage) {
       
       // Insertion de l'élément "article"
-
       let productArticle = document.createElement("article");
       document.querySelector("#cart__items").appendChild(productArticle);
       productArticle.className = "cart__item";
@@ -30,73 +35,58 @@ function getCart() {
       );
 
       // Insertion de l'élément "div"
-
       let productDivImg = document.createElement("div");
       productArticle.appendChild(productDivImg);
       productDivImg.className = "cart__item__img";
 
       // Insertion de l'image
-
       let productImg = document.createElement("img");
       productDivImg.appendChild(productImg);
       productImg.src = productLocalStorage[produit].productImg;
       productImg.alt = productLocalStorage[produit].altproductImg;
 
       // Insertion de l'élément "div"
-
       let productItemContent = document.createElement("div");
       productArticle.appendChild(productItemContent);
       productItemContent.className = "cart__item__content";
 
       // Insertion de l'élément "div"
-
       let productItemContentTitlePrice = document.createElement("div");
       productItemContent.appendChild(productItemContentTitlePrice);
-      productItemContentTitlePrice.className =
-        "cart__item__content__titlePrice";
+      productItemContentTitlePrice.className = "cart__item__content__titlePrice";
 
       // Insertion du titre h3
-
       let productTitle = document.createElement("h2");
       productItemContentTitlePrice.appendChild(productTitle);
       productTitle.innerText = productLocalStorage[produit].productName;
 
       // Insertion de la couleur
-
       let productColor = document.createElement("p");
       productTitle.appendChild(productColor);
       productColor.innerText = productLocalStorage[produit].productColor;
       productColor.style.fontSize = "20px";
 
       // Insertion du prix
-
       let productPrice = document.createElement("p");
       productItemContentTitlePrice.appendChild(productPrice);
       productPrice.innerText = productLocalStorage[produit].productPrice + " €";
 
       // Insertion de l'élément "div"
-
       let productItemContentSettings = document.createElement("div");
       productItemContent.appendChild(productItemContentSettings);
       productItemContentSettings.className = "cart__item__content__settings";
 
       // Insertion de l'élément "div"
-
       let productItemContentSettingsQuantity = document.createElement("div");
-      productItemContentSettings.appendChild(
-        productItemContentSettingsQuantity
-      );
-      productItemContentSettingsQuantity.className =
-        "cart__item__content__settings__quantity";
+      productItemContentSettings.appendChild(productItemContentSettingsQuantity);
+      productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
 
       // Insertion de "Qté : "
-
       let productQte = document.createElement("p");
       productItemContentSettingsQuantity.appendChild(productQte);
       productQte.innerText = "Qté : ";
 
       // Insertion de la quantité
-
       let productQuantity = document.createElement("input");
       productItemContentSettingsQuantity.appendChild(productQuantity);
       productQuantity.value = productLocalStorage[produit].productQuantity;
@@ -107,14 +97,11 @@ function getCart() {
       productQuantity.setAttribute("name", "itemQuantity");
 
       // Insertion de l'élément "div"
-
       let productItemContentSettingsDelete = document.createElement("div");
       productItemContentSettings.appendChild(productItemContentSettingsDelete);
-      productItemContentSettingsDelete.className =
-        "cart__item__content__settings__delete";
+      productItemContentSettingsDelete.className = "cart__item__content__settings__delete";
 
       // Insertion de "p" supprimer
-
       let productDeleted = document.createElement("p");
       productItemContentSettingsDelete.appendChild(productDeleted);
       productDeleted.className = "deleteItem";
@@ -127,10 +114,8 @@ getCart();
 function getTotals() {
 
   // Récupération du total des quantités
-
   let elemsQtt = document.getElementsByClassName("itemQuantity");
-  let myLength = elemsQtt.length,
-    totalQtt = 0;
+  let myLength = elemsQtt.length, totalQtt = 0;
 
   for (let i = 0; i < myLength; ++i) {
     totalQtt += elemsQtt[i].valueAsNumber;
@@ -138,10 +123,8 @@ function getTotals() {
 
   let productTotalQuantity = document.getElementById("totalQuantity");
   productTotalQuantity.innerText = totalQtt;
-  console.log(totalQtt);
 
   // Récupération du prix total
-
   totalPrice = 0;
 
   for (let i = 0; i < myLength; ++i) {
@@ -151,12 +134,10 @@ function getTotals() {
 
   let productTotalPrice = document.getElementById("totalPrice");
   productTotalPrice.innerText = totalPrice;
-  console.log(totalPrice);
 }
 getTotals();
 
 // Modification d'une quantité de produit
-
 function modifyQtt() {
   let qttModif = document.querySelectorAll(".itemQuantity");
 
@@ -173,7 +154,6 @@ function modifyQtt() {
       localStorage.setItem("produit", JSON.stringify(productLocalStorage));
 
       // refresh rapide
-
       location.reload();
     });
   }
@@ -181,7 +161,6 @@ function modifyQtt() {
 modifyQtt();
 
 // Suppression d'un produit
-
 function deleteProduct() {
   let btn_dlt = document.querySelectorAll(".deleteItem");
 
@@ -190,7 +169,6 @@ function deleteProduct() {
       event.preventDefault();
 
       //Selection de l'element à supprimer en fonction de son id ET sa couleur
-
       let idDelete = productLocalStorage[j].idProduct;
       let colorDelete = productLocalStorage[j].productColor;
 
@@ -201,64 +179,17 @@ function deleteProduct() {
       localStorage.setItem("produit", JSON.stringify(productLocalStorage));
 
       //Alerte produit supprimé et refresh
-
       alert("Ce produit a bien été supprimé du panier");
       location.reload();
     });
   }
 }
 deleteProduct();
-
-//Instauration formulaire avec regular expressions
-
-function getForm() {
-  // Ajout des Regex
-
-  let form = document.querySelector(".cart__order__form");
-
-  //Création des expressions régulières
-
-  let emailRegExp = new RegExp(
-    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$"
-  );
-  let charRegExp = new RegExp("^[a-zA-Z ,.'-]+$");
-  let addressRegExp = new RegExp(
-    "^([1-9][0-9]*(?:-[1-9][0-9]*)*)[\\s,-]+(?:(bis|ter|qua)[\\s,-]+)?([\\w]+[\\-\\w]*)[\\s,]+([-\\w].+)$"
-  );
-
-  // Ecoute de la modification du prénom
-
-  form.firstName.addEventListener("change", function () {
-    validFirstName(this);
-  });
-
-  // Ecoute de la modification du nom
-
-  form.lastName.addEventListener("change", function () {
-    validLastName(this);
-  });
-
-  // Ecoute de la modification de l'adresse
-
-  form.address.addEventListener("change", function () {
-    validAddress(this);
-  });
-
-  // Ecoute de la modification de la ville
-
-  form.city.addEventListener("change", function () {
-    validCity(this);
-  });
-
-  // Ecoute de la modification de l'email
-
-  form.email.addEventListener("change", function () {
-    validEmail(this);
-  });
+console.log(firstNameElt);
 
 /**
  * VALIDATION DU PRENOM
- * @param {string} firstNameElt 
+ * @param {object} firstNameElt 
  * @returns 
  */
 function validFirstName(firstNameElt) {
@@ -285,68 +216,73 @@ function validFirstName(firstNameElt) {
   }
 };
 
-  //validation du nom
-
+//validation du nom
 function validLastName(lastNameElt) {
-    let lastNameErrorMsg = lastNameElt.nextElementSibling;
+  let lastNameErrorMsg = lastNameElt.nextElementSibling;
 
-    if (charRegExp.test(lastNameElt.value)) {
-      lastNameErrorMsg.innerText = "";
-    } else {
-      lastNameErrorMsg.innerText = "Veuillez renseigner ce champ.";
-    }
-  };
+  if (charRegExp.test(lastNameElt.value)) {
+    lastNameErrorMsg.innerText = "";
+  } else {
+    lastNameErrorMsg.innerText = "Veuillez renseigner ce champ.";
+  }
+};
 
-  //validation de l'adresse
-
+//validation de l'adresse
 function validAddress (adressElt) {
-    let addressErrorMsg = adressElt.nextElementSibling;
+  const addressRegExp = new RegExp(
+    "^([1-9][0-9]*(?:-[1-9][0-9]*)*)[\\s,-]+(?:(bis|ter|qua)[\\s,-]+)?([\\w]+[\\-\\w]*)[\\s,]+([-\\w].+)$"
+  );
+  let addressErrorMsg = adressElt.nextElementSibling;
 
-    if (addressRegExp.test(adressElt.value)) {
-      addressErrorMsg.innerText = "";
-    } else {
-      addressErrorMsg.innerText = "Veuillez renseigner ce champ.";
-    }
-  };
+  if (addressRegExp.test(adressElt.value)) {
+    addressErrorMsg.innerText = "";
+  } else {
+    addressErrorMsg.innerText = "Veuillez renseigner ce champ.";
+  }
+};
 
-  //validation de la ville
-
+//validation de la ville
 function validCity (cityElt) {
-    let cityErrorMsg = cityElt.nextElementSibling;
+  let cityErrorMsg = cityElt.nextElementSibling;
 
-    if (charRegExp.test(cityElt.value)) {
-      cityErrorMsg.innerText = "";
-    } else {
-      cityErrorMsg.innerText = "Veuillez renseigner ce champ.";
-    }
-  };
+  if (charRegExp.test(cityElt.value)) {
+    cityErrorMsg.innerText = "";
+  } else {
+    cityErrorMsg.innerText = "Veuillez renseigner ce champ.";
+  }
+};
 
-  //validation de l'email
-
+//validation de l'email
 function validEmail (mailElt) {
-    let emailErrorMsg = mailElt.nextElementSibling;
+  const emailRegExp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$"
+  );
+  let emailErrorMsg = mailElt.nextElementSibling;
 
-    if (emailRegExp.test(mailElt.value)) {
-      emailErrorMsg.innerText = "";
-    } else {
-      emailErrorMsg.innerText = "Veuillez renseigner votre email.";
-    }
-  };
+  if (emailRegExp.test(mailElt.value)) {
+    emailErrorMsg.innerText = "";
+  } else {
+    emailErrorMsg.innerText = "Veuillez renseigner votre email.";
+  }
+};
+
+function checkForm() {
+  console.log("1 => ", typeof validFirstName(), validFirstName(firstName))
+  if (!validFirstName(firstNameElt) || !validLastName(lastNameElt) || !validAddress(adressElt) || !validCity(cityElt) || !validEmail(mailElt)) {
+}
 }
 
 //Envoi des informations client au localstorage
-
+console.log(firstNameElt);
 function postForm() {
   const btn_order = document.getElementById("order");
+  console.log(firstNameElt);
 
   //Ecouter le panier
-  
-  btn_order.addEventListener("click", () => {
-    let firstName = firstNameElt.value ;
-      if (!validFirstName(firstName) || !lastName || !address || !city || !email) {
-    }
-    //Construction d'un array depuis le local storage
+  btn_order.addEventListener("click", checkForm);
 
+    //Construction d'un array depuis le local storage
+    console.log(productLocalStorage);
     let idProducts = [];
     for (let i = 0; i < productLocalStorage.length; i++) {
       idProducts.push(productLocalStorage[i].idProduct);
@@ -355,7 +291,7 @@ function postForm() {
 
     const order = {
       contact: {
-        firstName: firstName,
+        firstName: firstNameElt.value,
         lastName: lastNameElt.value,
         address: adressElt.value,
         city: cityElt.value,
@@ -385,6 +321,5 @@ function postForm() {
       .catch((err) => {
         alert("Problème avec fetch : " + err.message);
       });
-  });
 }
 postForm();
